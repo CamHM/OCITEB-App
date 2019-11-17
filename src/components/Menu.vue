7896f4420d21d4b05d3b60d8d3cd9e219e74668b
fabianramos01 <fabian.ramos01@uptc.edu.co>
2019-11-16 22:57:47 -0500

    Componente menu, falta agregarle estilos

---
+++ src/components/Menu.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-container class="container">
+        <v-banner single-line>
+            OCITEB
+        </v-banner>
+        <v-btn block color="primary">
+            <v-icon left>mdi-home-variant-outline</v-icon>
+            Inicio
+        </v-btn>
+        <v-list dense>
+            <v-subheader>{{listTitle}}</v-subheader>
+            <v-list-group v-for="item in items" :key="item.title" v-model="item.active"
+                          :prepend-icon="item.icon">
+                <template v-slot:activator>
+                    <v-list-item-content class="style-black">
+                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                    </v-list-item-content>
+                </template>
+
+                <v-list-item v-for="subItem in item.items" :key="subItem.title">
+                    <v-list-item-content>
+                        <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+        </v-list>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "menu",
+        data() {
+            return {
+                listTitle: "Área",
+                items: [
+                    {
+                        title: 'Facultades',
+                        icon: 'mdi-checkbox-multiple-blank-outline ',
+                        items: [
+                            {title: 'List Item'},
+                        ],
+                    },
+                    {
+                        title: 'Escuelas',
+                        icon: 'mdi-dock-left',
+                        active: false,
+                        items: [
+                            {title: 'List Item'},
+                            {title: 'List Item'},
+                            {title: 'List Item'}
+                        ],
+                    },
+                    {
+                        title: 'Investigadores',
+                        icon: 'mdi-account',
+                        items: [
+                            {title: 'List Item'},
+                        ],
+                    },
+                    {
+                        title: 'Grupos de invetigación',
+                        icon: 'mdi-account-group',
+                        items: [
+                            {title: 'List Item'},
+                        ],
+                    }
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .container {
+        background: #1f232c;
+        margin-left: unset;
+        margin-top: unset;
+    }
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#f7b345',
+                secondary: '#1f232c',
+            },
+        },
+    }
 });
