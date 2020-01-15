355dee6adc777590fa32ecb2034546584a0cb0b6
Camila Andrea Herrera Melo <cami.andre469@gmail.com>
2020-01-15 00:32:18 -0500

    Mejora de CardIndicators

--- src/components/home/CardIndicators.vue
+++ src/components/home/CardIndicators.vue
@@ -1,131 +1,145 @@
 <template>
     <v-card class="card mx-auto" elevation="5"
             v-bind:color="item.card_color" :height="item.card_height">
-        <div class="text-left">
-            <v-card-text>
-                <h4>{{ item.title }}</h4>
-                <b>{{ item.description }}</b>
-                <v-row>
-                    <v-col cols="6">
-                        <v-btn color="primary" class="homeButton">
-                            Conoce los indicadores
-                        </v-btn>
-                    </v-col>
-                    <v-col cols="6">
-
-                    </v-col>
-                </v-row>
-                <v-divider class="white"/>
-                <v-row>
-                    <v-col cols="6">
-                        <ProgressIndicator v-bind:indicator="investment"/>
-                        <!--                    <h6>{{investment.title}}</h6>-->
-                        <!--                    <v-progress-linear rounded color="blue darken-3" background-color="white"-->
-                        <!--                                       v-model="investment.num"/>-->
-                    </v-col>
-                    <v-col cols="6">
-                        <h6>{{capacity.title}}</h6>
-                        <v-progress-linear rounded color="blue darken-3" background-color="white"
-                                           v-model="capacity.num"/>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="6">
-                        <div>
-                            <h6>{{formation.title}}</h6>
-                            <v-progress-linear rounded color="blue darken-3" background-color="white"
-                                               v-model="formation.num"/>
-                        </div>
-                    </v-col>
-                    <v-col cols="6">
-                        <h6>{{production.title}}</h6>
-                        <v-progress-linear rounded color="blue darken-3" background-color="white"
-                                           v-model="production.num"/>
-                    </v-col>
-                </v-row>
-            </v-card-text>
-        </div>
+        <v-row class="firstRow" no-gutters>
+            <v-col cols="6" class="firstCol">
+                <div>
+                    <h4><b>{{ item.title }}</b></h4>
+                    <p>{{ item.description }}</p>
+                </div>
+                <v-btn small color="primary" class="homeButton">
+                    Conoce los indicadores
+                </v-btn>
+            </v-col>
+            <v-col cols="6" class="secondCol">
+                <img src="../../assets/indicators.jpg" width="235">
+            </v-col>
+        </v-row>
+        <v-divider class="darken-2 grey"> </v-divider>
+        <v-row class="secondRow" no-gutters>
+            <v-col cols="6" class="thirdCol endPosition">
+                <div class="indicatorProgress">
+                    <h6>{{investment.title}}</h6>
+                    <v-progress-linear rounded color="#7567f0" background-color="white"
+                                       v-model="investment.num"/>
+                </div>
+                <div class="indicatorProgress">
+                    <h6>{{formation.title}}</h6>
+                    <v-progress-linear rounded color="#ff9e43" background-color="white"
+                                       v-model="formation.num"/>
+                </div>
+            </v-col>
+            <v-col cols="6" class="fourthCol endPosition">
+                <div class="indicatorProgress">
+                    <h6>{{capacity.title}}</h6>
+                    <v-progress-linear rounded color="#ea5454" background-color="white"
+                                       v-model="capacity.num"/>
+                </div>
+                <div class="indicatorProgress">
+                    <h6>{{production.title}}</h6>
+                    <v-progress-linear rounded color="#28c76f" background-color="white"
+                                       v-model="production.num"/>
+                </div>
+            </v-col>
+        </v-row>
     </v-card>
 </template>
 
 <script>
-    import ProgressIndicator from "./ProgressIndicator";
+import ProgressIndicator from "./ProgressIndicator";
 
-    export default {
-        name: "Card",
-        props: [
-            'item',
-        ],
-        components: [
-            ProgressIndicator
-        ],
-        methods: {},
-        data: () => {
-            return {
-                investment: {
-                    title: "Inversión",
-                    num: 12,
-                    color: "deep-purple darken-1"
-                },
-                capacity: {
-                    title: "Capacidad",
-                    num: 35,
-                    color: "deep-purple darken-1"
-                },
-                formation: {
-                    title: "Formación",
-                    num: 20,
-                    color: "deep-purple darken-1"
-                },
-                production: {
-                    title: "Producción bibliográfica",
-                    num: 70,
-                    color: "deep-purple darken-1"
-                }
+export default {
+    name: "Card",
+    props: [
+        'item',
+    ],
+    components: [
+        ProgressIndicator
+    ],
+    methods: {},
+    data: () => {
+        return {
+            investment: {
+                title: "Inversión",
+                num: 50,
+                color: "#7567f0"
+            },
+            capacity: {
+                title: "Capacidad",
+                num: 50,
+                color: "deep-purple darken-1"
+            },
+            formation: {
+                title: "Formación",
+                num: 50,
+                color: "deep-purple darken-1"
+            },
+            production: {
+                title: "Producción bibliográfica",
+                num: 50,
+                color: "deep-purple darken-1"
             }
         }
     }
+}
 </script>
 
 <style scoped>
     .card {
-        text-align: center;
-        box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
-        transition: all .3s ease-in-out;
+        padding: 20px;
+        color: white;
+    }
+    .card:hover {
+        cursor: context-menu;
+    }
+    .firstRow {
+        height: 75%;
+        padding-bottom: 10px;
+    }
+
+    .secondRow {
+        height: 25%;
+    }
+
+    .firstCol {
         display: flex;
         flex-direction: column;
-        align-content: center;
-        justify-content: center;
+        justify-content: space-between;
+        padding-bottom: 5px;
+        padding-right: 10px;
     }
-
-    .card:hover {
-        cursor: pointer;
-    }
-
-    h4 {
-        font-size: 18px;
-        font-weight: 600;
-        color: white;
-        margin-bottom: 10px;
-    }
-
-    b {
-        color: white;
-        font-size: 10px;
-        font-weight: lighter;
-    }
-
-    .homeButton {
-        text-transform: capitalize;
-        width: 90%;
-        padding: 0;
-        font-family: Calibri, Serif, sans-serif;
+    .secondCol {
         display: flex;
-        margin-left: 10px;
+        padding-left: 10px;
     }
-
+    .thirdCol {
+        padding-right: 10px;
+    }
+    .fourthCol {
+        padding-left: 10px;
+    }
+    .endPosition {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+    }
+    .indicatorProgress {
+        margin-bottom: 5px;
+    }
+    .homeButton {
+        font-size: 11px;
+        text-transform: lowercase;
+    }
+    h4 {
+        font-size: 17px;
+    }
+    p {
+        color: lightgray;
+        font-size: 11px;
+    }
     h6 {
-        color: white;
-        align-self: flex-start;
+        color: lightgray;
+        font-size: 11px;
+        font-weight: lighter;
     }
 </style>
